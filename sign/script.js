f910efeca2a4fd0249a0929e65398ba751bf7348
Navneet <navneet.shahi2004@gmail.com>
2023-08-30 17:04:59 +0530

    sign page

---
+++ sign/script.js
@@ -0,0 +1,31 @@
+let up = document.getElementById("Up")
+let field = document.getElementById("field")
+let title = document.getElementById("title")
+let In = document.getElementById("In")
+let sub = document.getElementById("sub")
+let forget = document.getElementById("forget")
+
+
+In.onclick = function(){
+    field.style.maxHeight = "0"
+    title.innerHTML = "SignIn"
+    up.classList.add("disable")
+    In.classList.add("color")
+    In.classList.remove("disable")
+    up.classList.remove("color")
+    // sub.classList.remove("move")
+    forget.classList.add("hidden")
+    sub.classList.add("moveup")
+}
+up.onclick = function(){
+    field.style.maxHeight = "65px"
+    title.innerHTML = "SignUp"
+    In.classList.add("disable")
+    up.classList.add("color")
+    up.classList.remove("disable")
+    In.classList.remove("color")
+    title.classList.add("trans")
+    // sub.classList.add("move")
+    forget.classList.remove("hidden")
+    sub.classList.remove("moveup")
+}
